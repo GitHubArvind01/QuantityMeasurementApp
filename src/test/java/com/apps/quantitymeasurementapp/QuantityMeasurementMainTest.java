ecc40d9d6fb95fe188d31e84d3bf502951a9abaf
GitHubArvind01 <kumararvind21390@gmail.com>
2026-02-20 16:37:11 +0530

    [ARVIND] : Added the program file of FeetEquality

--- src/test/java/com/apps/quantitymeasurementapp/QuantityMeasurementMainTest.java
+++ src/test/java/com/apps/quantitymeasurementapp/QuantityMeasurementMainTest.java
@@ -8,35 +8,35 @@ import org.junit.jupiter.api.Test;
 class QuantityMeasurementMainTest {
 
 	@Test
-	public void testEquality_SameValue()  {
+	public void testFeetEquality_SameValue()  {
 		FeetEquality feet1 = new FeetEquality(10.1);
 		FeetEquality feet2 = new FeetEquality(10.1);
 		assertTrue(feet1.equals(feet2));
 	}
 	
 	@Test
-	public void testEquality_DifferentValue() {
+	public void testFeetEquality_DifferentValue() {
 		FeetEquality feet1 = new FeetEquality(10.1);
 		FeetEquality feet2 = new FeetEquality(1.1);
 		assertFalse(feet1.equals(feet2));
 	}
 
 	@Test
-	public void testEquality_NullComparison() {
+	public void testFeetEquality_NullComparison() {
 		FeetEquality feet1 = new FeetEquality(1.0);
 		FeetEquality feet2 = null;
 		assertFalse(feet1.equals(feet2));;
 	}
 	
 	@Test
-	public void testEquality_NonNumericInput() {
+	public void testFeetEquality_NonNumericInput() {
 		assertThrows(IllegalArgumentException.class, ()-> {
 			new FeetEquality(Double.NaN);
 		});
 	}
 	
 	@Test
-	public void testEquality_SameReference() {
+	public void testFeetEquality_SameReference() {
 		FeetEquality feet1 = new FeetEquality(1.0);
 		FeetEquality feet2 = feet1;
 		assertTrue(feet1.equals(feet2));
