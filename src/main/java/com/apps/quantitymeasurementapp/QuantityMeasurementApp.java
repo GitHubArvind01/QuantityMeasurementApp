4f8df095db3d9529acc705f458cb9a43d1f904d7
GitHubArvind01 <kumararvind21390@gmail.com>
2026-02-20 12:28:33 +0530

    [ARVIND] : initial setup add of quantitymeasurement

---
+++ src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -0,0 +1,13 @@
+package com.apps.quantitymeasurementapp;
+
+public class QuantityMeasurementApp {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static class Feet{
+		//private final double value;
+		
+	}
+}
