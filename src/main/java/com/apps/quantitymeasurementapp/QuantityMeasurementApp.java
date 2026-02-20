1218cde073327801119b297b41bd921b44ba1477
GitHubArvind01 <kumararvind21390@gmail.com>
2026-02-20 16:56:15 +0530

    [ARVIND] : Added the program file of InchEquality

--- src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -13,9 +13,7 @@ public class QuantityMeasurementApp {
 			}
 			this.value = value;
 		}
-		public Double getValue() {
-			return value;
-		}
+
 		@Override
 		public boolean equals(Object obj) {
 			if(this==obj) {
@@ -25,13 +23,44 @@ public class QuantityMeasurementApp {
 			if(obj==null || obj.getClass()!=this.getClass()) {
 				return false;
 			}
-			return Double.compare(this.value, ((FeetEquality)obj).getValue())==0;
+			return Double.compare(this.value, ((FeetEquality)obj).value)==0;
 		}
 	}
 	
+	public static class InchEquality{
+		private final double value;
+
+		public InchEquality(double value) {
+			if(Double.isNaN(value)) {
+				throw new IllegalArgumentException("Invalid input!");
+			}
+			this.value = value;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(this==obj) {
+				return true;
+			}
+			if(obj==null || obj.getClass()!=this.getClass()) {
+				return false;
+			}
+			return Double.compare(this.value, ((InchEquality)obj).value)==0;
+		}
+	}
+	
+	public static boolean domonstrateInchEquality() {
+		InchEquality inch1 = new InchEquality(23.2);
+		InchEquality inch2 = new InchEquality(23.2);
+		return inch1.equals(inch2);
+	}
+	public static boolean domonstrateFeetEquality() {
+		FeetEquality feet1 = new FeetEquality(34.2);
+		FeetEquality feet2 = new FeetEquality(34.2);
+		return feet1.equals(feet2);
+	}
 	public static void main(String[] args) {
-		FeetEquality feet1 = new FeetEquality(23.2);
-		FeetEquality feet2 = new FeetEquality(23.2);
-		System.out.println(feet1.equals(feet2));
+		domonstrateFeetEquality();
+		domonstrateInchEquality();
 	}
 }
--- src/test/java/com/apps/quantitymeasurementapp/QuantityMeasurementMainTest.java
+++ src/test/java/com/apps/quantitymeasurementapp/QuantityMeasurementMainTest.java
@@ -1,12 +1,13 @@
 package com.apps.quantitymeasurementapp;
-import com.apps.quantitymeasurementapp.QuantityMeasurementApp.FeetEquality;
+import com.apps.quantitymeasurementapp.QuantityMeasurementApp.FeetEquality; 
+import com.apps.quantitymeasurementapp.QuantityMeasurementApp.InchEquality;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class QuantityMeasurementMainTest {
-
+	
 	@Test
 	public void testFeetEquality_SameValue()  {
 		FeetEquality feet1 = new FeetEquality(10.1);
@@ -40,5 +41,41 @@ class QuantityMeasurementMainTest {
 		FeetEquality feet1 = new FeetEquality(1.0);
 		FeetEquality feet2 = feet1;
 		assertTrue(feet1.equals(feet2));
+	}
+	
+
+	@Test
+	public void testInchEquality_SameValue()  {
+		InchEquality Inch1 = new InchEquality(10.1);
+		InchEquality Inch2 = new InchEquality(10.1);
+		assertTrue(Inch1.equals(Inch2));
+	}
+	
+	@Test
+	public void testInchEquality_DifferentValue() {
+		InchEquality Inch1 = new InchEquality(10.1);
+		InchEquality Inch2 = new InchEquality(1.1);
+		assertFalse(Inch1.equals(Inch2));
+	}
+
+	@Test
+	public void testInchEquality_NullComparison() {
+		InchEquality Inch1 = new InchEquality(1.0);
+		InchEquality Inch2 = null;
+		assertFalse(Inch1.equals(Inch2));;
+	}
+	
+	@Test
+	public void testInchEquality_NonNumericInput() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			new InchEquality(Double.NaN);
+		});
+	}
+	
+	@Test
+	public void testInchEquality_SameReference() {
+		InchEquality Inch1 = new InchEquality(1.0);
+		InchEquality Inch2 = Inch1;
+		assertTrue(Inch1.equals(Inch2));
 	}
 }
