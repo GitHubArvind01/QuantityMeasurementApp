2fbc3e6c48a2055a53a2226fc28f1fdd13e5b166
GitHubArvind01 <kumararvind21390@gmail.com>
2026-03-22 12:05:46 +0530

    [ARVIND] : Added the Customer Details Service file to load user from ddb

---
+++ src/main/java/com/app/quantitymeasurementapp/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.app.quantitymeasurementapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.app.quantitymeasurementapp.model.User;
+import com.app.quantitymeasurementapp.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+
+        User user = repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
