07ffe44f15db6a6f3abfb65e0ff670203a04173a
GitHubArvind01 <kumararvind21390@gmail.com>
2026-03-23 11:51:11 +0530

    [ARVIND] : updated the security layer of stateless session

--- src/main/java/com/app/quantitymeasurementapp/config/SecurityConfig.java
+++ src/main/java/com/app/quantitymeasurementapp/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,6 +14,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.app.quantitymeasurementapp.exception.JwtAuthenticationEntryPoint;
 
 import lombok.AllArgsConstructor;
 
@@ -32,10 +35,11 @@ public class SecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 				.csrf(csrf -> csrf.disable()) // - THIS IS KEY!
+				.exceptionHandling(exception-> exception.authenticationEntryPoint(new JwtAuthenticationEntryPoint()))
 				.authorizeHttpRequests(authz -> authz
 						.requestMatchers("/auth/user/**", "/swagger-ui/**", "/h2-console/**", "/v3/api-docs/**").permitAll()
-						.anyRequest().authenticated()
-						);
+						.anyRequest().authenticated())
+						.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		
 		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 		
